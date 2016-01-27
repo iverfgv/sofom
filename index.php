ce6274cba51e84256347d9d06a13ed4d1c778428
Herrera Lopez Jose Raul <raul_herreralsc@hotmail.com>
2016-01-27 15:27:27 -0600

    terminado

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title>Soluciones y Oportunidades</title>
+	<meta  name = "viewport"  content = "width = dispositivo de ancho, escala inicial = 1, de máxima escala = 1, el usuario escalable = no">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+
+   <div  class = "container colorFondo " >
+ 
+ <!--  cabecera inicio   -->
+       <div class= "row colorHeader" >
+       			
+       			<div class= "col-md-2 " ><img src="imagenes/logo.png" class="img-responsive" ></div>
+       			<div class= "col-md-3 titulo" >Soluciones y &nbsp;<span class="TituloSpam">Oportunidades</span> </div>
+       			<div class= "col-md-5 titulo2">Soluciones y Oportinudades SA de CV SOFOM ENR</div>
+ 	   </div>
+<!--  Cabecera fin    -->
+
+    <div class="separador"></div>
+
+<!--####### Menu ##### -->
+    <div class= "row colormenu"  >
+        
+        <div class="col-md-8 colormenu">
+        <div class="navbar-header">
+        	 <a class="navbar-brand" href="index.php">Empresa</a>
+        </div>
+        	
+          	<ul  class="nav navbar-nav">
+            	<li  class="active "><a href="#">Productos Financieros</a></li>
+            	<li><a href="#">Informacion</a></li>
+            	<li><a href="#">Aviso de privacidad</a></li>
+            	<li><a href="#">Contacto</a></li>
+            	<li class=""><a href="Galeria.html">Galeria</a></li>
+
+            </ul>
+        </div>
+    </div>
+
+<!--  Fin menu   -->
+
+<!--  slider    -->
+<section  id="miSlide" class="carousel slide row">
+                <ol class="carousel-indicators">
+                    <li data-target="#miSlide" data-slide-to="0" class="active"></li>
+                    <li data-target="#miSlide" data-slide-to="1"></li>
+                    <li data-target="#miSlide" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="imagenes/img1.jpg" class="adaptar">
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/img2.jpg" class="adaptar">
+                    </div>
+                    <div class="item">
+                        <img src="imagenes/img3.jpg" class="adaptar">
+                    </div>
+                </div>
+
+                <a href="#miSlide" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a href="#miSlide" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </section>
+     
+       
+  
+<!--termina slider-->
+
+<!--  Section Ofrecemos    -->
+    <div>
+    <img class="imgOfrecemos" src="imagenes/Ofrecemos.png" class="img-reponsive" width="220" height="120">
+    </div>
+<!-- fin   -->
+
+
+ 
+<div class="separadorHeder"></div>
+
+<!--  Section Texto   -->
+
+      <div class="col-md-6 colorTexto">
+       <p>This page is about the human-like machine; for Bot accounts on Wikipedia, see Wikipedia:Bots The Honda ASIMO robotA robot is an artificial agent, meaning it acts as a substitute for a person, doing things it is designed for.Robots are usually machines controlled by a computer program or electronic circuitry. They may be directly controlled by humans. They may be designed to look like humans, in which case their behaviour may suggest intelligence or thought. Most robots do a specific job, and they do not look like humans. They can come in many forms.[1] In fiction, however, robots usually look like people, and seem to have a life of their own.[2] There are many books, movies, and video games with robots in them. Isaac Asimov's I, Robot is perhaps the most famous.</p>
+       </div>
+  
+  <!--  Formulario     -->
+ <form class="formSolicitud" action="" methos="POST">
+       
+      
+      
+        <div class="col-xs-3  well positionForm">
+              <div class="LabelForm textSpam" > !SOLICITA UNA COTIZACIÓN AQUÍ!</div>
+              <label class="LabelForm"  form="nombre separacion">Nombre:</label>
+              <input type="text" class="form-control separacion" id="nombre" name="txtNombre" required autofocus>
+              <label class="LabelForm" form="email">Email:</label>
+              <input type="email" class="form-control separacion" id="email"  name="txtEmail" >
+              <label class="LabelForm" form="nombre">Monto aproximado a Solicitar:</label>
+              <input type="text" class="form-control separacion" id="monto" name="txtMonto" >
+              <label class="LabelForm" form="nombre">Ventas Mensuales Aproximadas:</label>
+              <input type="text" class="form-control separacion" id="xxxx" name="xxx" >
+              <label class="LabelForm" form="nombre">Oficio o Profesión:</label>
+              <input type="text" class="form-control separacion" id="profesion" name="txtProfesion" >
+              <label class="LabelForm" form="nombre">Destino del crédito:</label>
+              <input type="text" class="form-control separacion" id="destino" name="txtDestino" >
+              <label class="LabelForm" form="nombre">Información adicional:</label>
+              <textarea class="form-control  separacion" id="informacion" name="txtInformacion"></textarea>
+              <div class="col-xs-12">
+                <buttom class="btn btn-lg btn-primary btn-block btnSolicitar separacion" type="submit">Solicitar</buttom>
+              </div>
+           </div>
+  
+      </form>
+  
+<!--Fin Formulario   -->
+
+
+
+       <div class="separadorFooter"></div>
+
+
+
+<!--#### pie de pagina ####--> 
+    <div class= "row footer" >
+       
+       <div class="row">
+         <div class="col-md-6">
+            <div class="Datos">
+                  <div > Tel:(961) 618-1553 / (961) 618-1554 </div>
+                  <div>Calle 12 Poniente Norte 931-C</div>
+                  <div>Barrio Juy Juy</div>
+                  <div>CP. 29038</div>
+                  <div>Tuxtla Gutierrez, Chiapas, México.</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="siguenos"> &nbsp;&nbsp; Siguenos:</div>
+            <div class="col-md-2">
+              <img src="imagenes/googleMas.png" width="30" height="30">
+            </div>
+             <div class="col-md-2">
+             <img src="imagenes/iconofacebook.png" width="30" height="30">
+             </div>
+              <div class="col-md-2">
+               <img src="imagenes/iconotwitter.png" width="30" height="30">
+               </div>
+
+            
+          </div>
+        </div>
+
+
+
+   
+          
+    </div>
+<!--  Fin pie de pagina   -->
+          
+        	
+</div>
+
+ <script>window.jQuery || document.write('<script src="js/jquery-1.10.1.min.js"><\/script>')</script>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
