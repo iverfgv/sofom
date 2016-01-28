c284a1eb0ad9099b4c181e40fc5f6e073e4d3da9
Herrera Lopez Jose Raul <raul_herreralsc@hotmail.com>
2016-01-28 09:50:35 -0600

    nuevos archivos funcional

---
+++ contacto.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title>Soluciones y Oportunidades</title>
+	<meta  name = "viewport"  content = "width = dispositivo de ancho, escala inicial = 1, de máxima escala = 1, el usuario escalable = no">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/contacto.css">
+</head>
+<body>
+
+   <div  class = "container colorFondo" >
+  <!--  cabecera inicio   -->
+       <div class= "row colorHeader" >
+       			
+       			<div class= "col-md-2 " ><img src="imagenes/logo.png" width="120" height="120" class="img-rounded"></div>
+       			<div class= "col-md-3 titulo" >Soluciones y &nbsp;<span class="TituloSpam">Oportunidades</span> </div>
+       			<div class= "col-md-5 titulo2">Soluciones y Oportinudades SA de CV SOFOM ENR</div>
+ 	   </div>
+<!--  Cabecera fin    -->
+
+    <div class="separador"></div>
+    
+   <!--####### Menu ##### -->
+    <div class= "row colormenu"  >
+        
+        <div class="col-md-8 colormenu">
+        <div class="navbar-header">
+        	 <a class="navbar-brand" href="index.php">Empresa</a>
+        </div>
+        	
+          	<ul class="nav navbar-nav">
+            	<li class="active"><a href="#">Productos Financieros</a></li>
+            	<li><a href="#">Informacion</a></li>
+            	<li><a href="#">Aviso de privacidad</a></li>
+            	<li><a href="contacto.php">Contacto</a></li>
+            	<li><a href="Galeria.html">Galeria</a></li>
+
+            </ul>
+        </div>
+    </div>
+
+<!--  Fin menu   -->
+
+    <div class="row">
+    	<div class="col-xs-4"> 
+ 		</div>
+ 		
+ 		<div class="col-xs-4 contactanos"> 
+ 			CONTACTANOS
+ 		</div>
+		
+		<div class="col-xs-4"> 
+ 		</div>
+	</div>
+<!-- formulari de contactos -->
+	<div class="">
+	<div class="row fondoContenido">
+	   <form class="frmContacto" action="" methos="POST">
+		<div class="form-group">
+		
+		<div class="col-xs-5">
+			<input type="text" class="form-control separacion colorCajaTexto tamano" id="nombre" name="txtNombre" placeholder="Nombre" required autofocus>		
+			<input type="text" class="form-control separacion colorCajaTexto tamano" id="telefono" name="txtTelefono" placeholder="Telefono">
+			<input type="email" class="form-control separacion colorCajaTexto tamano" id="email" name="txtEmail" placeholder="E-mail">
+			<textarea class="form-control  separacion colorCajaTexto"rows="5" id="informacion" name="txtInformacion" placeholder="Mensaje"></textarea>
+		
+			<div class="col-xs-12">
+				<div class="col-xs-1">
+					<buttom class="btn btn-lg btn-primary btn-block btnSolicitar separacion" type="submit">ENVIAR</buttom>	
+				</div>
+			</div>
+		</div>
+		
+		</form>
+
+		<div class="col-xs-5 mapa">
+			<div class="embed-responsive embed-responsive-16by9">
+			<iframe class="embed-responsive-item" src="https://www.google.com/maps/
+			embed?pb=!1m18!1m12!1m3!1d3820.608669705969!2d-93.12898944621953!3d16.746
+			36652695039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
+			%3A0xaa644f1b9f519463!2sCalle+Doceava+Pte.+Sur%2C+Tuxtla+Guti%C3%A9rrez%2C+
+			Chis.!5e0!3m2!1ses-419!2smx!4v1453917852602"></iframe>
+			</div>
+		</div>
+		</div>
+		</div>
+	</div>
+
+<div class="col-md-8">
+      
+
+       </div>
+
+       </div> 
+       <!--#### pie de pagina ####--> 
+       
+        <footer class="container piePagina">
+        	<div id="sucursales">
+        	<p id="lblSucursal">SUCURSALES</p>
+        	</div>
+
+        	<div class="row informacionContactos">
+ 				<div class="col-md-4">
+ 					Tel:(961) 618-15-53/(961)618-1554<br>
+					Calle 12 Poniente Norte 931-C<br>
+					Bario Juy Juy<br>
+					C.P.29038<br>
+					Tuxtla Gutierrez, Chiapas, México
+ 				</div>
+  				<div class="col-md-4">
+  					Tel:(961) 618-15-53/(961)618-1554<br>
+					Calle 12 Poniente Norte 931-C<br>
+					Bario Juy Juy<br>
+					C.P.29038<br>
+					Tuxtla Gutierrez, Chiapas, México<br>
+  				</div>
+  				<div class="col-md-4">
+  					Tel:(961) 618-15-53/(961)618-1554<br>
+					Calle 12 Poniente Norte 931-C<br>
+					Bario Juy Juy<br>
+					C.P.29038<br>
+					Tuxtla Gutierrez, Chiapas, México
+  				</div>
+			</div>
+
+
+			<div class="row-fluid">
+   				<div class="span12"></div>
+   				<div class="span12"></div>
+   				<div class="span12"></div>
+			</div>
+
+			<div class="row contactoSiguenos">
+ 				<div class="col-md-4">
+  					Tel:(961) 618-15-53/(961)618-1554<br>
+					Calle 12 Poniente Norte 931-C<br>
+					Bario Juy Juy<br>
+					C.P.29038<br>
+					Tuxtla Gutierrez, Chiapas, México
+  				</div>
+  				
+  			<div class="col-md-5">
+  				
+  					<div class="col-md-6"></div>
+  					<div class="col-md-2 lblSiguenos">Siguenos:</div>
+  					
+  				
+
+  				<div class="row contactoSiguenos">
+  						<div class="col-md-2">
+ 					</div>		
+					<div class="col-md-2">
+ 					</div>
+
+  					<div class="col-md-2">
+ 						<img src="imagenes/iconoGoogleMasBlanco.png" class="img-responsive img-rounded" alt="">
+ 					</div>
+ 				
+ 					<div class="col-md-2">
+ 						<img src="imagenes/iconoFacebookBlanco.png" class="img-responsive img-rounded" alt="">
+ 					</div>
+ 				
+ 					<div class="col-md-2">
+ 						<img src="imagenes/iconoTwitterBlanco.png" class="img-responsive img-rounded" alt="">
+ 					</div>
+  				</div>
+
+			</div>
+
+			<br>
+			<br>
+
+		</footer>
+        	
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+
+
+</html>
